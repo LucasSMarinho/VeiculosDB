f4dc02aaa044c7f03bd9bd0d59a2e8d02d175990
lucas S.M <lucas.marinho.ceu@gmail.com>
2026-06-21 21:30:58 -0300

    subindo projeyto com tela login e cadastro

--- veiculosnext/app/telaCadastrar/page.tsx
+++ veiculosnext/app/telaCadastrar/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import '../app/Login.css'
+import '@/app/telaCadastrar/TelaCadastrar.css'
 
 import Logo from "@/public/vectra.svg"
 import api from "@/services/services"
@@ -11,6 +11,7 @@ import { jwtDecode } from "jwt-decode"
 import { useRouter } from "next/navigation"
 import imagemEmail from '@/public/imagemEmail.svg'
 import imagemSenha from '@/public/imagemSenha.svg'
+import imagemUsuario from '@/public/imagemUsuario.svg'
 import Link from "next/link"
 import Image from 'next/image'
 
@@ -18,6 +19,7 @@ const Home = () => {
 
     const router = useRouter()
     const [senha, setSenha] = useState("")
+    const [nome, setNome] = useState("")
     const [email, setEmail] = useState("")
     const {token, setToken, usuario, setUsuario} = useContext(UsuarioContext)
 
@@ -49,6 +51,33 @@ const checarToken = (token : any) => {
         localStorage.removeItem("token")
     }
     }
+
+    const funcCadastrar = async () => {
+        console.log("Email:", email);
+        console.log("Senha:", senha);
+        console.log("Nome:", nome);
+     
+        try
+        {   
+            const objLogin = {
+                email: email,
+                senha: senha,
+                nome: nome
+            }
+
+            const response = await api.post('/Usuario', objLogin);
+            const tokenObtido = response.data.token;
+            
+            funcEntrar()
+
+            checarToken(tokenObtido)
+
+        } 
+        catch (error) 
+        {
+            console.error("Erro ao fazer cadastro:", error);
+        }
+    } 
 
     const funcEntrar = async () => {
         console.log("Email:", email);
@@ -85,9 +114,16 @@ const checarToken = (token : any) => {
             <Image src={Logo} alt="Logo do Filmoteca" width={300} />
             <form action="" className="form_login">
                 <div className='texto_login'>
-                <hr /><h1>Login</h1><hr />
+                <hr /><h1>Cadastro</h1><hr />
                 </div>
                 <div className="campos_login">
+                    <div className="campo_input">
+                        <label htmlFor="nome">Nome</label>
+                        <div>
+                        <Image src={imagemUsuario} alt="imagem nome" width={35}/>
+                        <input type="text" name="nome" placeholder="Digite seu nome" value={nome} onChange={(e) => setNome(e.target.value)}  />
+                        </div>
+                    </div>
                     <div className="campo_input">
                         <label htmlFor="email">E-mail</label>
                         <div>
@@ -103,8 +139,8 @@ const checarToken = (token : any) => {
                         </div>
                     </div>
                 </div>
-                <button className='botao_login' type="button" onClick={() => {funcEntrar()}}> Entrar </button>
-                <Link className="link_perfil" href="/telaCadastrar">  Não tem uma conta? <strong>Cadastre-se</strong> </Link>
+                <button className='botao_login' type="button" onClick={() => {funcCadastrar()}}> Cadastrar </button>
+                <Link className="link_perfil" href="/">  Já tem uma conta? <strong>Logar</strong> </Link>
                 
             </form>
           </section>
